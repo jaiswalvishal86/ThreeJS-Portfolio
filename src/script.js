29be5ea5e6cdb030101eacd60910f06b44d50e1b
Vishal Jaiswal <vishal@Vishals-MacBook-Air.local>
2024-02-10 23:44:34 +0530

    integrated matter-js physics engine in the footer

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import Matter from "matter-js";
 // import * as dat from "lil-gui";
 import gsap from "gsap";
 import vertex from "./shaders/test/vertex.glsl";
@@ -15,6 +16,8 @@ const loadingBarElement = document.querySelector(".loading-bar");
 
 const magneticCircle = document.getElementById("magneticCircle");
 const allLinks = document.querySelectorAll("a");
+const matterContainer = document.querySelector("#matter-container");
+const THICCNESS = 40;
 
 // Function to move the magnetic circle to the cursor position
 function moveMagneticCircle(event) {
@@ -325,29 +328,12 @@ requestAnimationFrame(raf);
 
 THREE.ColorManagement.enabled = false;
 
-/**
- * Debug
- */
-// const gui = new dat.GUI();
-
-const parameters = {
-  materialColor: "#d9d9d9",
-};
-
-/**
- * Base
- */
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
 
 // Scene
 const scene = new THREE.Scene();
 const sceneCopy = new THREE.Scene();
-
-/**
- * Objects
- */
-const debugObject = {};
 
 /**
  * Texture Loader
@@ -384,7 +370,6 @@ const treeShaderMaterial = new THREE.ShaderMaterial({
 const treePlaneGeometry = new THREE.PlaneGeometry(6, 6, 500, 500);
 
 //Meshes
-const objectDistance = 4;
 const mesh1 = new THREE.Mesh(treePlaneGeometry, treeShaderMaterial);
 
 scene.add(mesh1);
@@ -494,3 +479,157 @@ const tick = () => {
 };
 
 tick();
+
+const renderCanvas = () => {
+  let Engine = Matter.Engine,
+    Render = Matter.Render,
+    Runner = Matter.Runner,
+    Bodies = Matter.Bodies,
+    Composite = Matter.Composite;
+
+  // create an engine
+  let engine = Engine.create();
+
+  // create a renderer
+  let render = Render.create({
+    element: matterContainer,
+    engine: engine,
+    options: {
+      width: matterContainer.clientWidth,
+      height: matterContainer.clientHeight,
+      background: "transparent",
+      wireframes: false,
+      showAngleIndicator: false,
+    },
+  });
+
+  const boxSize = 50;
+  const boxCount = 50;
+  const boxes = [];
+
+  for (let i = 0; i < boxCount; i++) {
+    const box = Bodies.circle(
+      Math.random() * matterContainer.clientWidth,
+      -i * 100,
+      Math.random() * boxSize,
+      // boxSize,
+      {
+        friction: 0.3,
+        frictionAir: 0.00001,
+        restitution: 0.8,
+        render: {
+          fillStyle: "#ffc5b5",
+        },
+      }
+    );
+    boxes.push(box);
+    Composite.add(engine.world, box);
+  }
+
+  // for (let i = 0; i < 100; i++) {
+  //   let circle = Bodies.circle(i, 10, 30, {
+  //     friction: 0.3,
+  //     frictionAir: 0.00001,
+  //     restitution: 0.8,
+  //   });
+  //   Composite.add(engine.world, circle);
+  // }
+
+  let ground = Bodies.rectangle(
+    matterContainer.clientWidth / 2,
+    matterContainer.clientHeight + THICCNESS / 2,
+    27184,
+    THICCNESS,
+    { isStatic: true }
+  );
+
+  let leftWall = Bodies.rectangle(
+    0 - THICCNESS / 2,
+    matterContainer.clientHeight / 2,
+    THICCNESS,
+    matterContainer.clientHeight * 5,
+    {
+      isStatic: true,
+    }
+  );
+
+  let rightWall = Bodies.rectangle(
+    matterContainer.clientWidth + THICCNESS / 2,
+    matterContainer.clientHeight / 2,
+    THICCNESS,
+    matterContainer.clientHeight * 5,
+    { isStatic: true }
+  );
+
+  // add all of the bodies to the world
+  Composite.add(engine.world, [ground, leftWall, rightWall]);
+
+  let matterMouse = Matter.Mouse.create(render.canvas);
+  let mouseConstraint = Matter.MouseConstraint.create(engine, {
+    mouse: matterMouse,
+    constraint: {
+      stiffness: 0.2,
+      render: {
+        visible: false,
+      },
+    },
+  });
+
+  Composite.add(engine.world, mouseConstraint);
+
+  // allow scroll through the canvas
+  mouseConstraint.mouse.element.removeEventListener(
+    "mousewheel",
+    mouseConstraint.mouse.mousewheel
+  );
+  mouseConstraint.mouse.element.removeEventListener(
+    "DOMMouseScroll",
+    mouseConstraint.mouse.mousewheel
+  );
+
+  // run the renderer
+  Render.run(render);
+
+  // create runner
+  var runner = Runner.create();
+
+  // run the engine
+  Runner.run(runner, engine);
+
+  function handleResize(matterContainer) {
+    // set canvas size to new values
+    render.canvas.width = matterContainer.clientWidth;
+    render.canvas.height = matterContainer.clientHeight;
+
+    // reposition ground
+    Matter.Body.setPosition(
+      ground,
+      Matter.Vector.create(
+        matterContainer.clientWidth / 2,
+        matterContainer.clientHeight + THICCNESS / 2
+      )
+    );
+
+    // reposition right wall
+    Matter.Body.setPosition(
+      rightWall,
+      Matter.Vector.create(
+        matterContainer.clientWidth + THICCNESS / 2,
+        matterContainer.clientHeight / 2
+      )
+    );
+  }
+
+  window.addEventListener("resize", () => handleResize(matterContainer));
+};
+
+ScrollTrigger.create({
+  trigger: matterContainer,
+  onEnter: renderCanvas,
+  once: true,
+  start: "top top",
+  end: "bottom bottom",
+  // onLeave: myLeaveFunc,
+  // onEnterBack: myEnterFunc,
+  // onLeaveBack: myLeaveFunc
+});
